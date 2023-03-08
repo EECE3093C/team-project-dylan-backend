6e56812b12c680ae59bbe0cadd1c120cb5f0fca8
Ben Sabo <sabobc@mail.uc.edu>
2023-03-08 18:56:04 -0500

    #38 Added nested class 'job'
    Added a nested class for a job that a student would work. One student can have multiple jobs in a year

--- src/main.py
+++ src/main.py
@@ -11,23 +11,14 @@
 
 import getDataFromWeb
 import time
-
-class Student():
-    def __init__(self, rate, hoursPerWeek, weeks):
-        self.rate = rate
-        self.hoursPerWeek = hoursPerWeek
-        self.weeks = weeks
     
+class Student():
+    def __init__(self):
+        self.job = self.Job()
+
     def getData(self):
         # Get data from user input
         return getDataFromWeb.getData()
-    
-    def getSemesterIncome(self):
-        # Semester income calculator
-        x = self.rate
-        y = self.hoursPerWeek
-        z = self.weeks
-        return (x*y)*z
     
     def getYearlyIncome(self):
         # Calculate yearly income
@@ -36,9 +27,28 @@ class Student():
     def getBudgetBreakdown(self):
         # 50/30/20 calculator
         return
+    
+    class Job():
+        def __init__(self, numWeeks, hoursPerWeek, rate, avgRent, taxRate):
+            self.numWeeks = numWeeks
+            self.hoursPerWeek = hoursPerWeek
+            self.rate = rate
+            self.avgRent = avgRent
+            self.taxRate = taxRate
+        
+        def getRent(self):
+            self.avgRent = 0 # Placeholder, replace with function to get rent from website
+            return self.avgRent
+        
+        def getJobIncome(self):
+            # Semester income calculator
+            x = self.rate
+            y = self.hoursPerWeek
+            z = self.numWeeks
+            return (x*y)*z
 
-s = Student(20, 15)
-
-print(s.getSemesterIncome())
+n = 1 # Number of jobs input by the user for the year
+user = Student(n)
+print(user.jobs[0].numWeeks)
 
 #Interface with web, send data

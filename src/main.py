6650f69af5db10d03dd5044fd57ba589c1ceb621
Ben Sabo <sabobc@mail.uc.edu>
2023-03-09 23:42:28 -0500

    #38 Added more setup functions
    Added setup functions inside the nested 'Job' class and called them from the init function

--- src/main.py
+++ src/main.py
@@ -13,8 +13,12 @@ import getDataFromWeb
 import time
     
 class Student():
-    def __init__(self):
-        self.job = self.Job()
+    def __init__(self, numJobs):
+        self.numJobs = numJobs
+
+        self.jobs = []
+        for i in range(self.numJobs):
+            self.jobs.append(self.Job())
 
     def getData(self):
         # Get data from user input
@@ -22,19 +26,48 @@ class Student():
     
     def getYearlyIncome(self):
         # Calculate yearly income
-        return
+        sum = 0
+        for i in range(self.numJobs):
+            sum += self.jobs[i].getJobIncome()
+        return sum
     
     def getBudgetBreakdown(self):
         # 50/30/20 calculator
         return
     
     class Job():
-        def __init__(self, numWeeks, hoursPerWeek, rate, avgRent, taxRate):
-            self.numWeeks = numWeeks
-            self.hoursPerWeek = hoursPerWeek
-            self.rate = rate
-            self.avgRent = avgRent
-            self.taxRate = taxRate
+        def __init__(self):
+            # From User
+            self.numWeeks = 3
+            self.hoursPerWeek = 10
+            self.rate = 20
+            self.state = "Ohio"
+
+            # From web scraper
+            self.avgRent = 0
+            self.taxRate = 0
+            
+            # Get values for data
+            self.getUserInput()
+            self.webScraper(self.rate, self.numWeeks, self.hoursPerWeek, self.state)
+        
+        # Not sure if this is the way to do it with web interface. Is data passed per-job or all at once?
+        def getUserInput(self):
+            numWkFromWeb = 15
+            self.numWeeks = numWkFromWeb
+            return
+
+
+        # Parameters are data from user (web interface)
+        def webScraper(self, rate, weeks, hrPerWk, state):
+            # Run selenium, pass parameters
+
+            # rent = getDataFromWeb.getRent(state)
+
+            stateTax = 0.1
+            self.taxRate = stateTax
+
+            return
         
         def getRent(self):
             self.avgRent = 0 # Placeholder, replace with function to get rent from website
@@ -49,6 +82,6 @@ class Student():
 
 n = 1 # Number of jobs input by the user for the year
 user = Student(n)
-print(user.jobs[0].numWeeks)
+print(user.getYearlyIncome())
 
-#Interface with web, send data
+# Interface with web, send data
